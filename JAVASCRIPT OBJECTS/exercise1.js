901f3f496d31b552a1a32982a35931fbdbce68e7
Nakawukasandra <143192495+Nakawukasandra@users.noreply.github.com>
2024-03-06 10:35:31 -0900

    LECTURE5ASSIGNMENT

---
+++ JAVASCRIPT OBJECTS/exercise1.js
@@ -0,0 +1,26 @@
+//1.Alert "John" by extracting information from the person object.
+const person = {
+    firstName: "John",
+    lastName: "Doe"
+  };
+ console.log(person.firstName);
+
+
+
+ //2.Add the following property and value to the person object: country: Uganda.
+const person2 = {
+    firstName: "Jane",
+    lastName: "Doe"
+  };
+  person2.country = "Uganda";
+  console.log(person2);
+
+
+//3.Create an object called person with name = John, age = 50. Then, access the object to alert("John is 50").
+const person3 ={
+    name:"John",
+    age:  50
+};
+console.log(person3.name + " "  + "is" + " " + person3.age);
+
+  
---
+++ JAVASCRIPT OBJECTS/objects.js
@@ -0,0 +1,43 @@
+//const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+const cohort3 ={year:2023, total:36}
+const person = {name:"Nakawuka Sandra", age:"25",gender:"female"}
+const car = {model_name: 'volvo'}
+const cars = {"brand name": 'toyota'}
+console.log(car.model_name)
+console.log(cars["brand name"])
+
+
+const person1 = {
+    firstName: "Nakawuka",
+    lastName: "Sandra",
+    id: 5566,
+    fullName: function(){
+        return this.firstName + " " + this.lastName;
+
+    }
+}
+
+console.log(person1.fullName()); // Corrected this line to call the fullName method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
